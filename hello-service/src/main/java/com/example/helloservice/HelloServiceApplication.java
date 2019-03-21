9f8d8d488b9991c84543049f655e08b1046d4661
An21 <mesayahu@o2.pl>
2019-03-21 19:46:43 +0100

    initial commit 2

---
+++ hello-service/src/main/java/com/example/helloservice/HelloServiceApplication.java
@@ -0,0 +1,24 @@
+package com.example.helloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RequestMapping("/hello")
+public class HelloServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServiceApplication.class, args);
+	}
+
+	@GetMapping("")
+	public String hello() {
+		return "Hello World! :)";
+	}
+
+}
