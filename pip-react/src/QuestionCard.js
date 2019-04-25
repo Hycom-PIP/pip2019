10c308ab1355702cc92c3144c595aea1ca767198
TomczykIII <kusmierekt97@gmail.com>
2019-04-25 06:46:32 +0200

    generating json from inputs, (pagitation,moving and deleting question not implemented yet)

--- pip-react/src/QuestionCard.js
+++ pip-react/src/QuestionCard.js
@@ -1,17 +1,120 @@
 import React, { Component } from 'react';
-import { MDBIcon, MDBInput,MDBCardGroup, MDBCard, MDBRow, MDBContainer, MDBCol, MDBListGroup, MDBListGroupItem } from "mdbreact";
+import { MDBIcon, MDBBtn, MDBInput, MDBCardGroup, MDBCard, MDBRow, MDBContainer, MDBCol, MDBListGroup, MDBListGroupItem } from "mdbreact";
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
 
-class QuestionCard extends Component {
+class MultipleAnswer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: []
+    }
+    this.AddNewQuestion = this.AddNewQuestion.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  AddNewQuestion() {
+    let answwweeers = this.state.answers;
+    answwweeers.push("");
+    this.setState(
+      (prevState)=>({ answers: answwweeers })
+      ,this.props.func(this.state.answers));
+   // this.props.func(this.answers);
+   
+  }
+  handleChange(event) {
+    let index = event.target.id.replace('AnswerTextInput', '');
+    let AllAnwsers = this.state.answers;
+    AllAnwsers[index] = event.target.value;
+    this.setState((prevState)=>({ answers: AllAnwsers }),this.props.func(this.state.answers))
+  }
   render() {
     return (
+      <MDBContainer fluid id="MultiChoice"  class>
+        {(this.state.answers || []).map((values, index) =>
+          (
+            <MDBRow  className="pt-0">
+              <MDBCol size="auto" className="pr-0">
+                <div class="custom-control custom-checkbox ">
+                  <input type="checkbox" class="custom-control-input" id={"IsRequiredCheckbox" + index} />
+                  <label class="custom-control-label" style={{ color: "#757575" }} for={"IsRequiredCheckbox" + index}></label>
+                </div>
+              </MDBCol>
+              <MDBCol size="6" className="m-0 p-0" >
+                <MDBInput key={index} id={"AnswerTextInput" + index} onChange={this.handleChange} hint="Przykładowa odpowiedź" value={values} className="m-0 p-0 MinusMarginTop" />
+              </MDBCol>
+              <MDBCol size="auto" > <MDBIcon icon="trash-alt" /></MDBCol>
+            </MDBRow>
+          ))}
+        <MDBBtn onClick={this.AddNewQuestion} color="primary">Dodaj nową odpowiedź</MDBBtn>
+      </MDBContainer>
+    )
+  }
+}
+
+class QuestionCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRequiered: false,
+      questionType: "Short",
+      answers: [],
+      questionComponent: (
+        <MDBContainer>
+          <MDBInput hint="Przykładowa odpowiedź" className="p-0" type="textarea" rows="1" />
+        </MDBContainer>
+      )
+    }
+    this.questionTextChange = this.questionTextChange.bind(this);
+    this.questionDescriptionChange = this.questionDescriptionChange.bind(this);
+    this.isRequieredChange = this.isRequieredChange.bind(this);
+    this.questionTypeChange = this.questionTypeChange.bind(this);
+    this.addAnswers=this.addAnswers.bind(this);
+
+  }
+  questionTextChange(e) {
+    this.props.func(e.target.value,this.props.index,"questionText");
+  }
+  questionDescriptionChange(e) {
+    this.props.func(e.target.value,this.props.index,"questionDescription");
+  }
+  isRequieredChange(e) {
+    this.props.func(!this.state.isRequiered,this.props.index,"isRequiered");
+    this.setState((prevStats)=>({isRequiered: !prevStats.isRequiered}))
+  }
+  addAnswers(e)
+  {
+    this.setState((PrevState)=>({answers: e}),this.props.func(e,this.props.index,"Multi"))
+  }
+  questionTypeChange(e) {
+    this.props.func(e,this.props.index,"questionType");
+    this.setState({ questionTypeChange: e, answers: [] });
+    if (e === "Short" || e === "Long") {
+      this.setState({
+        questionComponent: (
+          <MDBContainer>
+            <MDBInput  hint="Przykładowa odpowiedź" className="p-0" type="textarea" rows="1" />
+          </MDBContainer>
+        )
+      });
+
+    }
+    else if (e === "Radio" || e === "Check") {
+      this.setState({ questionComponent: <MultipleAnswer func={this.addAnswers} /> })
+    }
+    else {
+      this.setState({ questionComponent: null });
+    }
+
+  }
+  
+  render() {
+    return (
+      
       <div >
         <div class="row">
           <div class="col-2 align-self-center">
-   
 
-            <MDBCard  className="text-center " >
+            <MDBCard className="text-center " >
               <MDBListGroup className="h-100  " >
                 <MDBListGroupItem  >
                   <MDBIcon icon="angle-up " />
@@ -27,12 +130,12 @@ class QuestionCard extends Component {
                 </MDBListGroupItem>
               </MDBListGroup>
             </MDBCard>
-    
+
           </div>
           <div class="col">
-            <MDBInput type="textarea" label="Pytanie" rows="2" />
+            <MDBInput type="textarea" onChange={this.questionTextChange} label="Pytanie" rows="2" />
             <div>
-              <MDBInput type="textarea" label="Opis pytania" rows="2" />
+              <MDBInput type="textarea" onChange={this.questionDescriptionChange} label="Opis pytania" rows="2" />
             </div>
           </div>
         </div>
@@ -40,61 +143,23 @@ class QuestionCard extends Component {
         <div class="d-flex justify-content-between align-items-end">
           <p class="d-flex " style={{ color: "#757575" }}>Odpowiedź</p>
           <div class="d-flex align-items-end">
-            <DropdownButton id="dropdown-basic-button" title="Typ odpowiedzi">
-              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            <DropdownButton id="dropdown-basic-button" onSelect={this.questionTypeChange} title="Typ odpowiedzi">
+              <Dropdown.Item eventKey='Short' >Short</Dropdown.Item>
+              <Dropdown.Item eventKey='Long'>Long</Dropdown.Item>
+              <Dropdown.Item eventKey='Radio'>Radio</Dropdown.Item>
+              <Dropdown.Item eventKey='Check'>Check</Dropdown.Item>
+
             </DropdownButton>;
           </div>
         </div>
         <MDBContainer id="AnswerHolder" className="block-example border">
-          {/* Short and long text answers */}
-          <MDBContainer>
-            <MDBInput hint="Przykładowa odpowiedź" className="p-0" type="textarea" rows="1" />
-          </MDBContainer>
-          <MDBContainer fluid id="MultiChoice" class>
-            <MDBRow>
-              <MDBCol size="auto" className="pr-0">
-                <div class="custom-control custom-checkbox ">
-                  <input type="checkbox" class="custom-control-input" id="IsRequiredCheckbox" />
-                  <label class="custom-control-label" style={{ color: "#757575" }} for="IsRequiredCheckbox"></label>
-                </div>
-              </MDBCol>
-              <MDBCol size="6" className="m-0 p-0" >
-                <MDBInput id="AnswerTextInput" hint="Przykładowa odpowiedź" className="m-0 p-0 zero-top-margin" />
-              </MDBCol>
-              <MDBCol size="auto" > <MDBIcon icon="trash-alt" /></MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol size="auto" className="pr-0">
-                <div class="custom-control custom-checkbox ">
-                  <input type="checkbox" class="custom-control-input" id="IsRequiredCheckbox" />
-                  <label class="custom-control-label" style={{ color: "#757575" }} for="IsRequiredCheckbox"></label>
-                </div>
-              </MDBCol>
-              <MDBCol size="6" className="m-0 p-0" >
-                <MDBInput id="AnswerTextInput" hint="Przykładowa odpowiedź" className="m-0 p-0 zero-top-margin" />
-              </MDBCol>
-              <MDBCol size="auto" > <MDBIcon icon="trash-alt" /></MDBCol>
-            </MDBRow>
-            <MDBRow>
-              <MDBCol size="auto" className="pr-0">
-                <div class="custom-control custom-checkbox ">
-                  <input type="checkbox" class="custom-control-input" id="IsRequiredCheckbox" />
-                  <label class="custom-control-label" style={{ color: "#757575" }} for="IsRequiredCheckbox"></label>
-                </div>
-              </MDBCol>
-              <MDBCol size="6" className="m-0 p-0" >
-                <MDBInput id="AnswerTextInput" hint="Przykładowa odpowiedź" className="m-0 p-0 zero-top-margin" />
-              </MDBCol>
-              <MDBCol size="auto" > <MDBIcon icon="trash-alt" /></MDBCol>
-            </MDBRow>
-          </MDBContainer>
+          {
+            this.state.questionComponent
+          }
         </MDBContainer>
-
         <div class="d-flex flex-row-reverse mt-3">
           <div class="custom-control custom-checkbox ">
-            <input type="checkbox" class="custom-control-input" id="IsRequiredCheckbox" />
+            <input checked={this.state.isRequiered} onChange={this.isRequieredChange} type="checkbox" class="custom-control-input" id="IsRequiredCheckbox" />
             <label class="custom-control-label" style={{ color: "#757575" }} for="IsRequiredCheckbox">Wymagane</label>
           </div>
         </div>
--- pip-react/src/index.js
+++ pip-react/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Component } from 'react';
 import QuestionCard from './QuestionCard.js';
 import { MDBBtn, MDBIcon, MDBContainer, MDBInput, MDBPagination, MDBPageItem, MDBPageNav, MDBCol, MDBRow } from "mdbreact";
 
@@ -7,67 +8,163 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './css/style.css';
+let skelet = {
+        questionText: "",
+        questionDescription: "",
+        isRequiered: false,
+        questionType: "short",
+        answers: []
 
+}
+class MainView extends Component {
+        constructor(props) {
+                super(props);
+                this.state = {
+                        oldVersionToken: null,
+                        surveyName: null,
+                        surveyDescription: null,
+                        questionList: [{
+                                questionText: "",
+                                questionDescription: "",
+                                isRequiered: false,
+                                questionType: "short",
+                                answers: []
+
+                        }],
+                        CurrentPage: 1,
+
+                };
+                this.AddQuestionCard = this.AddQuestionCard.bind(this);
+                this.ChangePage = this.ChangePage.bind(this);
+                this.AddPage = this.AddPage.bind(this);
+                this.generateJson = this.generateJson.bind(this);
+                this.HandleSurveyChange = this.HandleSurveyChange.bind(this);
+                this.HandleSurveyDescritpionChange = this.HandleSurveyDescritpionChange.bind(this);
+                this.FillQuestion = this.FillQuestion.bind(this);
+        }
+        FillQuestion(e, index, type) {
+                this.setState((prevState) => {
+                        let old = prevState.questionList;
+                        let temp = old[index];
+                        switch (type) {
+                                case "questionText":
+                                        temp.questionText = e;
+                                        break;
+                                case "questionDescription":
+                                        temp.questionDescription = e;
+                                        break;
+                                case "isRequiered":
+                                        temp.isRequiered = e;
+                                        break;
+                                case "questionType":
+                                        temp.questionType = e;
+                                        break;
+                                case "Multi":
+                                        temp.answers = e;
+                                        break;
+                        }
+                        old[index] = temp;
+                        return { questionList: old }
+                }
+                )
+
+
+        }
+        generateJson() {
+                console.log(JSON.stringify(this.state));
+        }
+        HandleSurveyChange(e) {
+                this.setState({ surveyName: e.target.value });
+        }
+        HandleSurveyDescritpionChange(e) {
+                this.setState({ surveyDescription: e.target.value });
+        }
+        
+        AddQuestionCard() {
+                this.setState((prevState) => {
+                        let old = prevState.questionList;
+                        old.push({
+                                questionText: "",
+                                questionDescription: "",
+                                isRequiered: false,
+                                questionType: "short",
+                                answers: []
+
+                        });
+                        return { questionList: old }
+                }
+                );
+        }
+        AddPage() {
+
+        }
+        ChangePage(e) {
+              
+        }
+        render() {
+                return (
+                        <MDBContainer>
+                                <MDBInput onChange={this.HandleSurveyChange} type="textarea" label="Tytuł" rows="2" />
+                                <MDBInput onChange={this.HandleSurveyDescritpionChange} type="textarea" label="Opis" rows="2" />
+                                <br />
+                                <MDBPagination className="d-flex justify-content-center" size="lg">
+                                        <MDBPageItem disabled>
+                                                <MDBPageNav aria-label="Previous">
+                                                        <span aria-hidden="true">Previous</span>
+                                                </MDBPageNav>
+                                        </MDBPageItem>
+                                        {/* {
+                                                this.state.Components.map((vaule, index) =>
+                                                        (<MDBPageItem onClick={this.ChangePage} key={index + 1} data-id={index + 1} >
+                                                                <MDBPageNav>
+                                                                        {index + 1}
+                                                                </MDBPageNav>
+                                                        </MDBPageItem>))
+                                        } */}
+                                        <MDBPageItem disabled>
+                                                <MDBPageNav aria-label="Previous">
+                                                        <span aria-hidden="true">Next</span>
+                                                </MDBPageNav>
+                                        </MDBPageItem>
+                                </MDBPagination>
+
+                                <div class="container">
+                                        <div class="row">
+                                                <div class="col-sm">
+                                                        {
+                                                                (this.state.questionList || []).map(
+                                                                        (question, value) =>
+                                                                                (
+                                                                                        <QuestionCard index={value} func={this.FillQuestion} />
+                                                                                )
+                                                                )
+                                                        }
+                                                </div>
+                                                {/* <div class="col-sm">
+                                                        {
+                                                                (this.state.Components[this.state.CurrentPage-1] || []).map(
+                                                                        (question, value) =>
+                                                                                (
+                                                                                        <React.Fragment key={value}>{question}</React.Fragment>
+                                                                                )
+                                                                )
+                                                        }
+                                                </div> */}
+                                        </div>
+                                </div>
+                                <br />
+                                <MDBBtn onClick={this.AddQuestionCard} color="primary">Dodaj nowe pytanie</MDBBtn>
+                                <div id="DivForButtons" class="CenterThenEnd">
+                                        <div>Empty object</div>
+                                        <MDBBtn onClick={this.AddPage} color="primary">Dodaj stronę</MDBBtn>
+
+                                        <MDBBtn onClick={this.generateJson} color="primary">Zakończ</MDBBtn>
+
+                                </div>
+                        </MDBContainer>)
+
+        }
+}
 ReactDOM.render(
-        <MDBContainer>
-                <MDBInput type="textarea" label="Tytuł" rows="2" />
-                <MDBInput type="textarea" label="Opis" rows="2" />
-                <br />
-                <MDBPagination className="d-flex justify-content-center" size="lg">
-                        <MDBPageItem disabled>
-                                <MDBPageNav aria-label="Previous">
-                                        <span aria-hidden="true">Previous</span>
-                                </MDBPageNav>
-                        </MDBPageItem>
-                        <MDBPageItem active>
-                                <MDBPageNav>
-                                        1
-                                </MDBPageNav>
-                        </MDBPageItem>
-                        <MDBPageItem>
-                                <MDBPageNav>2</MDBPageNav>
-                        </MDBPageItem>
-                        <MDBPageItem>
-                                <MDBPageNav>3</MDBPageNav>
-                        </MDBPageItem>
-                        <MDBPageItem disabled>
-                                <MDBPageNav aria-label="Previous">
-                                        <span aria-hidden="true">Next</span>
-                                </MDBPageNav>
-                        </MDBPageItem>
-                </MDBPagination>
-                <QuestionCard />
-                <br />
-                <MDBBtn color="primary">Dodaj nowe pytanie</MDBBtn>               
-                <MDBPagination className="d-flex justify-content-center" size="lg">
-                        <MDBPageItem disabled>
-                                <MDBPageNav aria-label="Previous">
-                                        <span aria-hidden="true">Previous</span>
-                                </MDBPageNav>
-                        </MDBPageItem>
-                        <MDBPageItem active>
-                                <MDBPageNav>
-                                        1
-                                </MDBPageNav>
-                        </MDBPageItem>
-                        <MDBPageItem>
-                                <MDBPageNav>2</MDBPageNav>
-                        </MDBPageItem>
-                        <MDBPageItem>
-                                <MDBPageNav>3</MDBPageNav>
-                        </MDBPageItem>
-                        <MDBPageItem disabled>
-                                <MDBPageNav aria-label="Previous">
-                                        <span aria-hidden="true">Next</span>
-                                </MDBPageNav>
-                        </MDBPageItem>
-                </MDBPagination>
-                <div id="DivForButtons" class="CenterThenEnd">
-                        <div>Empty object</div>
-                        <div> <button id="AddPageButton" type="button" class="btn btn-primary">Dodaj stronę</button>
-                        </div>
-                        <div> <button id="FinishButton" type="button" class="btn btn-primary">Zakończ</button>
-                        </div>
-                </div>
-        </MDBContainer>
-        , document.getElementById('root'));
+
+        <MainView />, document.getElementById('root'));
