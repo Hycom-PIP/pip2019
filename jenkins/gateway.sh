3eb577b28f14e5f2921bd74d5282591464cc6b51
Mateox <mateox97@op.pl>
2019-05-16 03:06:11 +0200

    Updated configs

--- jenkins/gateway.sh
+++ jenkins/gateway.sh
@@ -7,17 +7,15 @@ kill -9 $(lsof -i:8080 -t)
 java -jar ./gateway/target/*.jar &
 iteration=3
 IsDiscoveryRunning=$(wget --server-response localhost:8080/actuator/health 2>&1| grep -c 'HTTP/1.1 200')
-while [ $IsDiscoveryRunning -eq 0 -a $iteration -ge 0 ] ; do
-echo "Waiting 3 seconds for gateway service to start"
+for i in $(seq 1 $iteration); do
+echo "Waiting 3 seconds for survey-service service to start"
+if [ $IsDiscoveryRunning -eq 0 ] ; then
+echo "Survey service is up"
+exit 0
+fi
 sleep 3
-(($iteration--))
 IsDiscoveryRunning=$(wget --server-response localhost:8080/actuator/health 2>&1| grep -c 'HTTP/1.1 200')
 done
-if [ $iteration -ge 0 ] ; then
-echo "Gateway service is up"
-exit 0
-else
-echo "Gateway service failed to start"
+echo "Survey service failed to start"
 exit 1
-fi
 #GATEWAY END
