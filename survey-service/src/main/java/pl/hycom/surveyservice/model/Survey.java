50544fc092884a44865fa3fdab8321a586d7271d
KamilJakubik <44211205+KamilJakubik@users.noreply.github.com>
2019-05-23 09:52:15 +0200

    changed oldVersionToken to token, added version number

--- survey-service/src/main/java/pl/hycom/surveyservice/model/Survey.java
+++ survey-service/src/main/java/pl/hycom/surveyservice/model/Survey.java
@@ -2,59 +2,67 @@ package pl.hycom.surveyservice.model;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public class Survey {
-
+    
     @Id
     private ObjectId id;
-    private String oldVersionToken;
+    private String token;
+    private int version;
     @NotBlank(message = "Survey Name cannot be null.")
     private String surveyName;
     private String surveyDescription;
     @NotNull(message = "Page cannot be null.")
     private @Valid Page[] pageList;
-
-    public String getOldVersionToken() {
-        return oldVersionToken;
+    
+    public String getToken () {
+        return token;
     }
-
-    public void setOldVersionToken(String oldVersionToken) {
-        this.oldVersionToken = oldVersionToken;
+    
+    public void setToken ( String token ) {
+        this.token = token;
     }
-
+    
+    public int getVersion ()    {
+        return version;
+    }
+    
+    public void setVersion ( int version )    {
+        this.version = version;
+    }
+    
     public String getSurveyName() {
         return surveyName;
     }
-
+    
     public void setSurveyName(String surveyName) {
         this.surveyName = surveyName;
     }
-
+    
     public String getSurveyDescription() {
         return surveyDescription;
     }
-
+    
     public void setSurveyDescription(String surveyDescription) {
         this.surveyDescription = surveyDescription;
     }
-
+    
     public Page[] getPageList() {
         return pageList;
     }
-
+    
     public void setPageList(Page[] pageList) {
         this.pageList = pageList;
     }
-
+    
     public ObjectId getId() {
         return id;
     }
-
+    
     public void setId(ObjectId id) {
         this.id = id;
     }
